13ffa51770bddfbbd56f47de8d6712ddae2e95ac
LeeJoNyun <115512931+LeeJoNyun@users.noreply.github.com>
2022-11-28 21:44:53 +0900

    second commit

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ var bodyParser = require('body-parser');
 
 
 //라우팅 모듈 선언
-
+var contact = require('./routes/contact')
 //express 서버 포트 설정
 app.set('port', process.env.PORT || 3000);
 
-//서버 생성
-app.listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-});
+//request 요청 URL과 처리 로직을 선언한 라우팅 모듈 매핑
+app.use('/contact', contact);
 
 //post 사용시 body-parser 설정
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()); // for parsing application/json
 
-//request 요청 URL과 처리 로직을 선언한 라우팅 모듈 매핑
-app.use('/', contact);
 
+//서버 생성
+app.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
--- routes/contact.js
+++ routes/contact.js
@@ -2,11 +2,37 @@ const {sql, sqlConfig} = require('../config/db')
 const express = require('express')
 const router = express.Router();
 
-router.get('/i_list', async (req,res) => {
+const list_ex = [
+    {key : "login_name", value : 'SANDY.CHEN'},
+    {key : "login_name", value : 'RMIN'},
+    {key : "login_name", value : 'SCHEN'},
+    {key : "login_name", value : 'SEANPARK'},
+    {key : "login_name", value : 'BPARK'},
+    {key : "login_name", value : 'SPHAN'},
+    {key : "login_name", value : 'CYI'},
+    {key : "employ_id", value : `'AA\'`},
+    {key : "employ_id", value : '587'},
+    {key : "employ_id", value : '589'},
+    {key : "employ_id", value : '079'},
+    {key : "employ_id", value : '813'},
+    {key : "employ_id", value : '170'},
+    {key : "employ_id", value : 'C02'},
+    {key : "employ_id", value : 'C67'},
+    {key : "employ_id", value : '557'},
+    {key : "employ_id", value : 'J47'},
+    {key : "employ_id", value : 'K53'},
+    {key : "employ_id", value : '640'},
+]
+
+router.get('/', async (req,res) => {
+
     try {
-        let pool = await sql.connect(config)
-        let result1 = await pool.request()
-            .query('')
+        let pool = await sql.connect(sqlConfig)
+        let result = await pool.request()
+            .query(`select 1`
+            )
+            
+            res.send(result)
     } catch (err) {
         res.status(500)
         res.send(err.message)
